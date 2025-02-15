1b6cfe45322b17f6ae034c6cbd984b81efb3192f
installer <installer@gmail.com>
2025-02-15 20:57:08 +0630

    changelayout

--- lua/nvconfig.lua
+++ lua/nvconfig.lua
@@ -6,7 +6,7 @@ local options = {
     hl_override = {},
     integrations = {},
     changed_themes = {},
-    transparency = true,
+    transparency = false,
     theme_toggle = { "onedark", "one_light" },
   },
 
@@ -18,21 +18,21 @@ local options = {
       },
       icons_left = false, -- only for non-atom styles!
       lspkind_text = true,
-      style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+      style = "flat_dark", -- default/flat_light/flat_dark/atom/atom_colored
       format_colors = {
-        tailwind = false, -- will work for css lsp too
+        tailwind = true, -- will work for css lsp too
         icon = "󱓻",
       },
     },
 
-    telescope = { style = "borderless" }, -- borderless / bordered
+    telescope = { style = "bordered" }, -- borderless / bordered
 
     statusline = {
       enabled = true,
-      theme = "minimal", -- default/vscode/vscode_colored/minimal
+      theme = "default", -- default/vscode/vscode_colored/minimal
       -- default/round/block/arrow separators work only for default statusline theme
       -- round and block will work for minimal theme only
-      separator_style = "block",
+      separator_style = "default",
       order = nil,
       modules = nil,
     },
