4377fb7dba17cec81ee97d1166d6e5e7a3c7ddc7
maotseantonio <thetzinantonio@gmail.com>
2025-04-17 09:04:08 +0630

    add-blink-cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,6 +5,60 @@ return {
     opts = require "configs.conform",
   },
 
+{
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  -- use a release tag to download pre-built binaries
+  version = '1.*',
+  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'default' },
+
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = { documentation = { auto_show = false } },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "prefer_rust_with_warning" }
+  },
+  opts_extend = { "sources.default" }
+},
+  { "chrisgrieser/nvim-genghis" },
 -- nvim v0.8.0
  {
     "kdheepak/lazygit.nvim",
@@ -198,13 +252,15 @@ return {
     "rcarriga/nvim-notify",
     }
 },
-  -- These are some examples, uncomment them if you want to see them work!
-  {
+
+ {
     "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
+     dependencies = { 'saghen/blink.cmp' },
+   config = function()
+    require "configs.lspconfig"
     end,
   },
+  -- These are some examples, uncomment them if you want to see them work! 
 
   ---@type LazySpec
 {
