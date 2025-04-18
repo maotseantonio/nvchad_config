8cd18997479a312619c974c284deb6dd606c9384
installer <installer@gmail.com>
2025-04-18 20:59:01 +0630

    new-lualine

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -30,10 +30,10 @@ local options = {
 
     statusline = {
       enabled = true,
-      theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+      theme = "default", -- default/vscode/vscode_colored/minimal
       -- default/round/block/arrow separators work only for default statusline theme
       -- round and block will work for minimal theme only
-      separator_style = "default",
+      separator_style = "block",
       order = nil,
       modules = nil,
     },
