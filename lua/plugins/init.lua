d714582a634511e23b1159cfb13390c1df37d147
maotseantonio <thetzinantonio@gmail.com>
2025-07-02 09:31:46 +0630

    new update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,6 +6,17 @@ return {
   },
    
   { "chrisgrieser/nvim-genghis" },
+{
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = function(_, opts)
+		-- Other blankline configuration here
+		return require("indent-rainbowline").make_opts(opts)
+	end,
+	dependencies = {
+		"TheGLander/indent-rainbowline.nvim",
+	},
+},
 -- nvim v0.8.0
  {
     "kdheepak/lazygit.nvim",
